7c6c20e0df2a6eb821e68b3ab3de7b7ccda5b469
U29 <yuluthi@gmail.com>
2024-10-20 17:34:05 +0900

    feat: ログ出力

--- src/mf_register_bot/mf_me_automation.py
+++ src/mf_register_bot/mf_me_automation.py
@@ -4,6 +4,16 @@ import os
 from dotenv import load_dotenv
 from pybit.unified_trading import HTTP
 import requests
+import logging
+
+# ログ設定
+log_file_path = "logs/mf.log"
+os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
+logging.basicConfig(
+    filename=log_file_path,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 # 環境変数をロード
 load_dotenv()
@@ -33,7 +43,7 @@ async def moneyforward_login():
         coin="USDT",
     )
     total_balance = float(total_balance["result"]["list"][0]["totalEquity"])
-    print(total_balance)
+    logging.info(f"Total balance: {total_balance}")
 
     # APIで為替レートを取得
     url = "https://api.excelapi.org/currency/rate?pair=usd-jpy"
@@ -43,6 +53,7 @@ async def moneyforward_login():
 
     # 総資産を円換算
     total_balance_jpy = total_balance * usd_rate
+    logging.info(f"Total balance in JPY: {total_balance_jpy}")
 
     # ブラウザ起動
     browser = await launch(
@@ -52,38 +63,42 @@ async def moneyforward_login():
             "--disable-gpu",
         ],
     )
-    print("Browser launched")
+    logging.info("Browser launched")
     page = await browser.newPage()
-    print("Page created")
+    logging.info("Page created")
 
+    # User-Agentをセット
     await page.setUserAgent(
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.88 Safari/537.36"
     )
-    print("User agent set")
+    logging.info("User agent set")
+
+    # Viewportを設定
     await page.setViewport({"width": 1200, "height": 1080})
-    print("Viewport set")
+    logging.info("Viewport set")
+
     # https://moneyforward.com にアクセス
     await page.goto(MONEYFORWARD_ACCOUNTS_URL)
-    print("Accessed to MoneyForward")
+    logging.info("Accessed to MoneyForward")
 
     # type="email" の input タグにメールアドレスを入力
     await page.waitForSelector('input[type="email"]')
     await page.type('input[type="email"]', MONEYFORWARD_EMAIL)
-    print("Typed email")
+    logging.info("Typed email")
 
     # id="submitto" の部分をクリック
     await page.click("#submitto")
-    print("Clicked submitto")
+    logging.info("Clicked submitto")
 
     # type="password" の input タグにパスワードを入力
     await page.waitForSelector('input[type="password"]')
     await page.type('input[type="password"]', MONEYFORWARD_PASSWORD)
-    print("Typed password")
+    logging.info("Typed password")
 
     # id="submitto" の部分をクリック
     await page.waitForSelector("#submitto")
     await page.click("#submitto")
-    print("Clicked submitto")
+    logging.info("Clicked submitto")
 
     await asyncio.sleep(5)
 
@@ -92,17 +107,17 @@ async def moneyforward_login():
     total_assets = await page.evaluate(
         'document.querySelector(".heading-small").textContent'
     )
-    print(total_assets)
+    logging.info(f"Total assets (raw): {total_assets}")
     total_assets = total_assets.split("：")[1]
     total_assets = total_assets.replace("円", "").replace(",", "")
-    print(total_assets)
+    logging.info(f"Total assets (formatted): {total_assets}")
 
     # 総資産の差分を計算
     diff = int(total_balance_jpy - float(total_assets))
-    print(diff)
+    logging.info(f"Asset difference: {diff}")
 
     if diff == 0:
-        print("資産に変動はありません")
+        logging.info("資産に変動はありません")
         return
 
     # class="cf-new-btn btn modal-switch btn-warning" のbuttonタグをクリック
@@ -117,13 +132,13 @@ async def moneyforward_login():
         # class=minus-payment かつ id=user_asset_act_payment_2 のinputタグをクリック
         await page.waitForSelector(".minus-payment")
         await page.evaluate('document.querySelector(".minus-payment").click()')
-
+        logging.info("Clicked expenditure button")
     else:
         # 収入ボタンをクリック
         # class=plus-payment かつ id=user_asset_act_payment_2 のinputタグをクリック
-
         await page.waitForSelector(".plus-payment")
         await page.evaluate('document.querySelector(".plus-payment").click()')
+        logging.info("Clicked income button")
         await asyncio.sleep(1)
 
     # id=appendedPrependedInput のinputタグに diff を入力
@@ -143,7 +158,7 @@ async def moneyforward_login():
         # id=19517320のaタグをクリック(その他)
         await page.waitForSelector('[id="19517320"]')
         await page.evaluate("document.querySelector('[id=\"19517320\"]').click()")
-        print("19517320 click end")
+        logging.info("Selected 'その他'")
 
         # id=js-middle-category-selected のaタグをクリック
         await page.waitForSelector("#js-middle-category-selected")
@@ -154,11 +169,12 @@ async def moneyforward_login():
         # id=19517320 のaタグをクリック(暗号資産取引)
         await page.waitForSelector('[id="19517320"]')
         await page.evaluate("document.querySelector('[id=\"19517320\"]').click()")
-
+        logging.info("Selected '暗号資産取引'")
     else:
         # id=1のaタグをクリック(収入)
         await page.waitForSelector('[id="1"]')
         await page.evaluate("document.querySelector('[id=\"1\"]').click()")
+        logging.info("Selected '収入'")
 
         await asyncio.sleep(1)
 
@@ -171,14 +187,14 @@ async def moneyforward_login():
         # id=19517314 のaタグをクリック(暗号資産取引)
         await page.waitForSelector('[id="19517314"]')
         await page.evaluate("document.querySelector('[id=\"19517314\"]').click()")
+        logging.info("Selected '暗号資産取引'")
 
         await asyncio.sleep(1)
 
     # id=submit-button のinputタグをクリック
     await page.waitForSelector("#submit-button")
     await page.evaluate('document.querySelector("#submit-button").click()')
-
-    print("資産を追加しました")
+    logging.info("資産を追加しました")
 
     # ブラウザを閉じる
     await browser.close()
